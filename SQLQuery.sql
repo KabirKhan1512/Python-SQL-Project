44797acd05b5f61f1ce176c7d877bae7d3b872ab
KabirKhan1512 <kabirkhan1512@gmail.com>
2025-03-11 02:19:41 +0500

    final

--- SQLQuery.sql
+++ SQLQuery.sql
@@ -9,6 +9,52 @@ create table crypto_info (
 
 select * from crypto_info;
 
+-- List the top 10 crypto coins with the highest USD market capitalization (usd_market_cap), ordered from highest to lowest.
 select top (10) *
 from crypto_info
 order by usd_market_cap desc;
+
+-- Calculate the average 24-hour trading volume (usd_24h_vol) across all coins.
+select avg(usd_24h_vol) as trading_volume
+from crypto_info;
+
+-- Identify the coin with the highest positive 24-hour percentage change (usd_24h_change) and the coin with the lowest (most negative) 24-hour percentage change.
+select coins, usd_24h_change
+from crypto_info
+where usd_24h_change = (select MAX(usd_24h_change) from crypto_info)
+or usd_24h_change = (select MIN(usd_24h_change) from crypto_info);
+
+-- For each coin, calculate the ratio of usd_market_cap to usd_24h_vol and order the results in descending order by this ratio.
+select coins, 
+case 
+	when usd_24h_vol = 0 then null
+	else (usd_market_cap/usd_24h_vol) 
+	end as MC_Vol_Ratio
+from crypto_info;
+
+-- Count how many coins experienced a 24-hour percentage change (usd_24h_change) of less than -5%.
+select coins, usd_24h_change
+from crypto_info
+where usd_24h_change < -5;
+
+-- Use a window function to rank the coins by usd_market_cap and display each coin’s name along with its rank.
+
+
+-- Calculate the total usd_market_cap and usd_24h_vol for all coins, then compute the percentage of the total market cap that each coin contributes.
+SELECT 
+    coins, 
+    CAST(usd_market_cap AS FLOAT) / NULLIF((SELECT SUM(usd_market_cap) FROM crypto_info), 0) AS mc_contribution, 
+    CAST(usd_24h_vol AS FLOAT) / NULLIF((SELECT SUM(usd_24h_vol) FROM crypto_info), 0) AS vol_contribution
+FROM crypto_info
+ORDER BY mc_contribution DESC;
+
+-- Show coins with 24h price changes is greater than the average volatility, ordered by market cap
+select coins, usd_24h_change
+from crypto_info
+where abs(usd_24h_change) > (select AVG(ABS(usd_24h_change)) from crypto_info)
+order by usd_24h_change;
+
+
+
+
+
